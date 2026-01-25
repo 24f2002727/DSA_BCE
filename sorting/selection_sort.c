ab2e02dcdf93c251890dc504f2ce6b19f6c42c94
24f2002727 <shivamrock811307@gmail.com>
2026-01-25 17:22:41 +0530

    selection sort completed

--- sorting/selection_sort.c
+++ sorting/selection_sort.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 void display(int arr[],int n)
 {
@@ -12,20 +11,23 @@ int main()
 {
     int arr[5]={1,8,0,4,9};  
     int n=5;
+
+    int min=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-i-1;j++)
+        for(int j=i+1;j<n;j++)
         {
-            if(arr[j+1]<arr[j])
+            if(arr[j]<arr[min])
             {
-                
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                min=j;
             }
         }
-    }    
-    
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+
+
     display(arr,5);
     return 0;
 }
