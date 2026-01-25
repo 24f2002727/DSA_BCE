7a880e879c45243b498e2b4753a789157ee6c174
24f2002727 <shivamrock811307@gmail.com>
2026-01-25 17:01:04 +0530

    bubble sort completed

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -10,29 +10,22 @@ void display(int arr[],int n)
 
 int main()
 {
-    int arr[5]={1,8,6,4,9};
+    int arr[5]={1,8,0,4,9};  //here 0 is converted to 1 and order is descending
     int n=5;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-i;j++)
+        for(int j=0;j<n-i-1;j++)
         {
-            if(arr[j]<arr[j+1])
+            if(arr[j+1]<arr[j])
             {
-                arr[j]=arr[j]+arr[j+1];
-                arr[j+1]=arr[j]-arr[j+1];
-                arr[j]-=arr[j+1];
-                // printf("%d ",arr[j]);
-                // printf("%d ",arr[j+1]);
-                // int temp=arr[j];
-                // arr[j]=arr[j+1];
-                // arr[j+1]=temp;
+                
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }    
-    // for(int i=0;i<n;i++){
-    //     printf("%d ",arr[i]);
-    // }
-
+    
     display(arr,5);
     return 0;
 }
