34858f8ce9ebdc4025154dc50b2cac34903b4c4a
24f2002727 <shivamrock811307@gmail.com>
2026-01-28 16:13:02 +0530

    Recursive quick sort completed

---
+++ sorting/quick_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+//used for swapping the values of array hence it is taken in the 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int left, int right) {
+    int pivot = arr[left];   // pivot element
+    int i = left - 1;   //index of smaller element
+
+    for (int j = left; j < right; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[left]);   //it is used for swapping the pivot elem at correct position
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (left < right)  //it is checking the condn for elem in the list>1
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
---
+++ sorting/tower_of_hanoi.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void tower(int,char[],char[],char[]);
+
+void main()
+{
+    int n;
+    printf("Enter the no of disks to be transfered:\t");
+    scanf("%d",&n);
+    if(n<1)
+    {
+        printf("\nIncorrect value");
+        exit(0);
+    }
+    else
+    {
+        printf("\nThe following moves are required for N=%d\n",n);
+        tower(n,"beg","aux","end");
+    }
+    return;
+}
+
+void tower(int num,char A[5],char B[5],char C[5])
+{
+    if(num==1)
+    {
+        printf("%s -> %s\t",A,C);
+        return;
+    }
+    tower(num-1,A,C,B);
+    printf("%s -> %s\t",A,C);
+    tower(num-1,B,A,C);
+    return;
+}
